9a095857ef4017ed16d532f857f6d48e8ee45a0b
GrigoryCode <grigorycode@gmail.com>
2019-05-30 21:20:48 +0300

    Add ItemList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import ItemList from './components/ItemList/ItemList';
+
 import './App.scss';
 
 function App() {
-  return <Navbar />;
+  return  <main>
+    <Navbar />
+    <ItemList />
+  </main>;
 }
 
 export default App;
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ItemList.scss';
+
+export default function Navbar() {
+  return <section>
+    <h1>Items</h1>
+  </section>;
+}
+
