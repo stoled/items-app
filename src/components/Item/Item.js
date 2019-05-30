6bd84549309a9cc78bbc7e6bc71c5463880a56da
GrigoryCode <grigorycode@gmail.com>
2019-05-30 22:03:01 +0300

    Item component correction

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,23 +2,21 @@ import React from 'react';
 // import './ItemList.scss';
 
 export default function Item() {
-  return <article>
-    <div className="col-lg-4 col-md-6 mb-4">
-      <div className="card h-100">
-        <a href="/">
-          <img className="card-img-top" src="http://placehold.it/700x400" alt="" />
-        </a>
-        <div className="card-body">
-          <h4 className="card-title">
-            <a href="/">Item One</a>
-          </h4>
-          <h5>$24.99</h5>
-          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-        </div>
-        <div className="card-footer">
-          <small className="text-muted">★ ★ ★ ★ ☆</small>
-        </div>
+  return <div className="col-lg-4 col-md-6 mb-4">
+    <div className="card h-100">
+      <a href="/">
+        <img className="card-img-top" src="http://placehold.it/700x400" alt="" />
+      </a>
+      <div className="card-body">
+        <h4 className="card-title">
+          <a href="/">Item One</a>
+        </h4>
+        <h5>$24.99</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">★ ★ ★ ★ ☆</small>
       </div>
     </div>
-  </article>;
+  </div>;
 }
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -6,6 +6,7 @@ export default function ItemList() {
   return <div className="container mt-5">
     <div class="row">
       <Item />
+      <Item />
     </div>
   </div>;
 }
